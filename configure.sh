9e17082519786ac733dc8cf5bf63d1756d03d7a9
wadoro <wadoro@msn.com>
2024-04-05 22:09:50 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -18,7 +18,10 @@ rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
 
 echo 'src-git lucky https://github.com/gdy666/luci-app-lucky' >>feeds.conf.default
-echo 'src-git wadoro https://github.com/wadoro/openwrt-packages' >>feeds.conf.default
+#echo 'src-git wadoro https://github.com/wadoro/openwrt-packages' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git fichenx https://github.com/fichenx/openwrt-package' >>feeds.conf.default
 
 # iStore
 echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
