dd8b8346807619fe7be88cce4fc4b436010af2e9
wadoro <wadoro@msn.com>
2024-04-05 13:46:16 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -13,13 +13,6 @@ sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='$(TZ=UTC-8 date +%
 #4.编译的固件文件名添加日期
 sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=$(shell TZ=UTC-8 date "+%Y%m%d")-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 
-echo 'src-git fichenx https://github.com/fichenx/openwrt-package' >>feeds.conf.default
-git clone https://github.com/bootli/luci-app-v2ray-server.git package/v2ray
-echo 'src-git lucky https://github.com/gdy666/luci-app-lucky' >>feeds.conf.default
-
-# iStore
-echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
-
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
