0b5835738818ff1dcd0c4174b6da9c8d3832edb3
wadoro <wadoro@msn.com>
2024-04-05 13:47:21 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -3,6 +3,13 @@ git clone  https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
 git clone  https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
 git clone  https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
 
+echo 'src-git wadoro https://github.com/wadoro/openwrt-package' >>feeds.conf.default
+git clone https://github.com/bootli/luci-app-v2ray-server.git package/v2ray
+echo 'src-git lucky https://github.com/gdy666/luci-app-lucky' >>feeds.conf.default
+
+# iStore
+echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+
 # Theme
 # luci-theme-neobird
 git clone https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
